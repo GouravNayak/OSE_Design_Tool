cc135888be992cdea036e2b3b3cf1569327e34c6
Gourav Nayak <gourav22nayak@gmail.com>
2020-12-27 15:58:28 -0500

    Made changes in gruntfile.js to deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
-      }
+      },
 	      buildcontrol: {
           options: {
             dir: 'dist',
