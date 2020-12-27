b56949900f47133df8ce7535b5d97aa196b6d38a
Gourav Nayak <gourav22nayak@gmail.com>
2020-12-27 17:13:48 -0500

    made changes in index.html

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -8,51 +8,51 @@
     design_Tool_App.config(['$routeProvider',function($routeProvider) {
         $routeProvider
 			.when('/', {
-			    templateUrl : 'views/home.html'
+			    templateUrl : 'app/views/home.html'
 			})
             // route for the home page
             .when('/home', {
-                templateUrl : 'views/home.html'
+                templateUrl : 'app/views/home.html'
             })
 
             // route for the about page
             .when('/about', {
-                templateUrl : 'views/aboutus.html'
+                templateUrl : 'app/views/aboutus.html'
             })
 
             // route for the visual inspection page
             .when('/visualinspection', {
-                templateUrl : 'views/visualinspection.html'
+                templateUrl : 'app/views/visualinspection.html'
             })
             
             // route for the choice reaction time page
             .when('/choicereactiontime', {
-                templateUrl : 'views/choicereactiontime.html'
+                templateUrl : 'app/views/choicereactiontime.html'
             })
             
             // route for the fitts tapping page
             .when('/fittstapping', {
-                templateUrl : 'views/fittstapping.html'
+                templateUrl : 'app/views/fittstapping.html'
             })
             
             // route for the simple reaction time page
             .when('/simplereactiontime', {
-                templateUrl : 'views/simplereactiontime.html'
+                templateUrl : 'app/views/simplereactiontime.html'
             })
             
             // route for the learning curve page
             .when('/learningcurve', {
-                templateUrl : 'views/learningcurve.html'
+                templateUrl : 'app/views/learningcurve.html'
             })
             
             // route for the short term memory span page
             .when('/shorttermmemoryspan', {
-                templateUrl : 'views/shorttermmemoryspan.html'
+                templateUrl : 'app/views/shorttermmemoryspan.html'
             })
             
             // route for the short term memory span page
             .when('/strooptest', {
-                templateUrl : 'views/strooptest.html'
+                templateUrl : 'app/views/strooptest.html'
             })
             
 			.otherwise('/home');
@@ -63,11 +63,3 @@
     design_Tool_App.controller('mainController', function() {
         
     });
-
-/*    design_Tool_App.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
-    });
-
-    design_Tool_App.controller('visualinspectionController', function($scope) {
-        $scope.message = 'Look! I am visual inspection page.';
-    });*/
