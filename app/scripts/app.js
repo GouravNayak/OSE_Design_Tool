339f0b278f27e1954013d9a9b77ac37fa8923392
Gourav Nayak <gourav22nayak@gmail.com>
2020-12-27 15:55:24 -0500

    Initial version of AngularJS ready to deploy

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name oseErgoPlusDesignApp
+ * @description
+ * # oseErgoPlusDesignApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('oseErgoPlusDesignApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
